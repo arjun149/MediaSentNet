bfe6d12f721fbf5b6e825561d1a056c35ef9424d
Arjun Alva <69946331+arjun149@users.noreply.github.com>
2025-11-09 21:26:49 -0500

    Update sentiment_analysis.py

--- sentiment/sentiment_analysis.py
+++ sentiment/sentiment_analysis.py
@@ -6,7 +6,15 @@ sia = SentimentIntensityAnalyzer()
 reddit_df = pd.read_csv('../data/reddit_posts.csv')
 twitter_df = pd.read_csv('../data/twitter_posts.csv')
 
-reddit_df['text_combined'] 
+reddit_df['text_combined'] = reddit_df['title'].fillna('') + ' ' + reddit_df['text'].fillna('')
+twitter_df['text_combined'] = twitter.df['text']
+
+all_posts = pd.concat([reddit_df[['date', 'text_combined']], twitter_df[['data', 'text_combined']]])
+
+all_posts['sentiment'] = all_posts['text_combined'].apply(lambda x: sia.polarity_scores(x)['compound'])
+
+daily_sentiment = all_posts.groupby('date')['sentiment'].mean().reset_index()
+daily_sentiment.to_csv('../data/daily_sentiment.csv', index=False)
 
 
 
